06a570309feef437f64ad5dd49c50161cf99bf34
kirito2056 <hajin_2056@naver.com>
2023-03-22 15:08:12 +0900

    수업 끝

--- Java/20230322/lec_20230322.java
+++ Java/20230322/lec_20230322.java
@@ -1,2 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class lec_20230322 {
+
+    public static void main(String args[]) {
+
+    }
+
+    static void answer01() throws IOException {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.print("첫번째 숫자 : ");
+            int first = Integer.parseInt(in.readLine());
+            System.out.print("두번째 숫자 : ");
+            int second = Integer.parseInt(in.readLine());
+
+            for ( int i = first ; i < second ; i++) {
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
