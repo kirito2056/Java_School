e99fb118e318c5650e3d1320581df2d9a95e7ed8
kirito2056 <hajin_2056@naver.com>
2023-03-29 11:44:47 +0900

    20230328

---
+++ Java/20230328/lec_20230328.java
@@ -0,0 +1,23 @@
+public class lec_20230328 {
+
+    /**
+     * 클래스, 객체
+     *  - 객체, 재활용, 상속, 캡슐화, 다양성, 추상화
+     *
+     *  클래스 - 붕어빵 틀, 설계도
+     *  객체 - 붕어빵, 인스턴스, 면빨
+     *
+     *  클래스.
+     *
+     */
+
+
+
+
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+
+
+
+}
