e0421d93311cf5e4d1d3ab8bcf3365d273b09d76
kirito2056 <hajin_2056@naver.com>
2023-06-14 19:38:21 +0900

    20230614

---
+++ Java/20230613/PhotoCard_Class.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+
+public class PhotoCard_Class {
+    String name;
+    int price;
+
+    PhotoCard_Class() {
+        name = "";
+        price = 0;
+    }
+
+    PhotoCard_Class(String _name, int _price) {
+        name = _name;
+        price = _price;
+    }
+
+
+}
