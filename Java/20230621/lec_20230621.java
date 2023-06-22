3b04c28defa59e13904e5818192b3222a84ae3f0
kirito2056 <hajin_2056@naver.com>
2023-06-22 09:47:51 +0900

    lec_20230621

---
+++ Java/20230621/lec_20230621.java
@@ -0,0 +1,18 @@
+import java.io.File;
+
+public class lec_20230621 {
+    public static void main(String arys[]) {
+        try {
+            File file01 = new File("aaa.txt");
+//            file01.createNewFile();
+
+//            File file02 = new File("aaa");
+//            file02.mkdirs();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
